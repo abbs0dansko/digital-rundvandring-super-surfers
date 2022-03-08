fd0f36c04157203c5e8cb41fdda50615be3661a9
Arvid Kolsmyr <Arvid.Kolsmyr@abbindustrigymnasium.se>
2022-03-08 13:50:32 +0100

    canvas animation on click

--- Assets/Scripts/GetPlaneInformation.cs
+++ Assets/Scripts/GetPlaneInformation.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+ using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+
 public class GetPlaneInformation : MonoBehaviour
 {
-    //public GameObject plane;
+    public Image panel;
+    public Animation anim;
     string planeName;
     // Start is called before the first frame update
     void Start()
@@ -15,6 +20,22 @@ public class GetPlaneInformation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetMouseButtonDown(0)){
+            RaycastHit hit;
+            Ray temp_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if (Physics.Raycast(temp_ray, out hit)) {
+                Transform objectHit = hit.transform;
+                Debug.Log("object hit");
+                anim.Play();
+                Color c = panel.color;
+                c.a = 100;
+                panel.color = c;
+                // Do something with the object that was hit by the raycast.
+            }
+        }
+
         if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
